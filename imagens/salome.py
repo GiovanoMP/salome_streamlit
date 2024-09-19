7ce6a1cff50313c719cbde2225c88a28d0c115e1
Giovano M Panatta <162298143+GiovanoMP@users.noreply.github.com>
2024-09-19 13:05:54 -0300

    Add files via upload

---
+++ imagens/salome.py
@@ -0,0 +1,76 @@
+import streamlit as st
+
+# Título da página
+st.title('Salomé - Nicho, Segmento e Estilo')
+
+# Exibindo a imagem
+st.image('C:/Users/giova/OneDrive/Documentos/salome/imagem_salome.jpg', caption='Coleção Curvas de Movimento')
+
+# Texto principal do site
+st.subheader('Nicho, Segmento e Estilo')
+st.write("""
+Nosso empreendimento é direcionado a mulheres com poder aquisitivo elevado que buscam exclusividade em suas escolhas de moda. Essas mulheres valorizam a possibilidade de expressar sua elegância e sofisticação por meio de peças únicas que refletem sua personalidade igualmente única. O público-alvo é composto por mulheres cultas, criativas e inovadoras, com idade a partir de 30 anos, que apreciam roupas femininas que destacam sua individualidade.
+
+Nosso estilo é caracterizado por ser atemporal, romântico, natural, criativo e sensual. Buscamos oferecer peças que combinam conforto e beleza, permitindo que nossas clientes se sintam confiantes e autênticas em qualquer ocasião. Valorizamos designs que transcendem tendências passageiras, focando em qualidade e durabilidade.
+""")
+
+st.subheader('Perfil Detalhado do Cliente Alvo')
+st.write("""
+Nossas clientes são mulheres que equilibram com maestria suas vidas pessoais e profissionais. Com alto poder aquisitivo, elas têm um estilo de vida ativo e cosmopolita, frequentemente viajando a trabalho ou lazer. Muitas gerenciam seus próprios negócios ou ocupam posições de destaque em diversas áreas profissionais.
+
+Elas valorizam o bem-estar e o cuidado pessoal. Apreciam experiências culturais, visitar exposições de arte, participar de eventos sociais sofisticados, e prezam por produtos e serviços que proporcionem conforto, exclusividade e beleza. Nosso objetivo é atender a essas necessidades, oferecendo peças de vestuário que complementem e enriqueçam suas vidas dinâmicas e exigentes.
+""")
+
+st.subheader('Objetivo a Ser Alcançado')
+st.write("""
+O principal objetivo do nosso projeto é preservar e revitalizar a arte das vestimentas tradicionais brasileiras. Reconhecemos a importância cultural e histórica dessas peças e, portanto, enfatizamos a produção manual detalhada na confecção da maioria de nossas roupas. Ao valorizar o trabalho artesanal, buscamos assegurar que essa rica herança cultural seja mantida viva e apreciada pelas gerações atuais e futuras.
+
+Além disso, pretendemos conscientizar as pessoas sobre os benefícios dos tecidos naturais para o corpo. Materiais como algodão, linho e seda natural proporcionam conforto superior, promovem o bem-estar e mantêm o equilíbrio térmico do corpo. Ao destacar essas qualidades, encorajamos uma conexão mais profunda entre o indivíduo e o que ele veste.
+""")
+
+st.subheader('Objetivos que Determinam o Sucesso do Projeto')
+st.write("""
+- **Valorização da Cultura e Produtos Nacionais:** Desejamos que as mulheres se sintam lindas e empoderadas ao usar roupas desenvolvidas com produtos nacionais. Ao incorporar elementos da cultura, arte e raízes brasileiras em nossas peças, promovemos o apoio à economia local.
+  
+- **Apreço pelos Materiais Primos Brasileiros:** Nosso compromisso é destacar a qualidade e a beleza dos materiais primos do Brasil. Ao utilizar tecidos e recursos locais, não apenas valorizamos a indústria nacional, mas também garantimos produtos autênticos e de alta qualidade.
+
+- **Redefinição dos Padrões Estéticos:** Buscamos mostrar às mulheres que não é necessário seguir os padrões estéticos convencionais predominantes. Incentivamos a formação de uma identidade de moda genuinamente brasileira, que celebre a diversidade e individualidade de cada mulher.
+
+- **Empoderamento Feminino:** Acreditamos que é possível ser feminina e forte simultaneamente. Nossas roupas são projetadas para valorizar os corpos das mulheres, permitindo que elas expressem sua sensualidade e movimento com confiança e elegância.
+
+- **Experiência do Cliente:** Almejamos proporcionar uma experiência excepcional às nossas clientes, desde o atendimento personalizado até a qualidade impecável dos produtos. O sucesso será medido pela satisfação e fidelização das clientes, bem como pelo impacto positivo na comunidade e na indústria da moda brasileira.
+""")
+
+st.subheader('Mensagem do Projeto')
+st.write("""
+Nosso projeto busca transmitir a mensagem de que é plenamente possível para as mulheres serem fortes e femininas, criativas e elegantes, atemporais e românticas. Almejamos inspirar cada mulher a reconhecer e celebrar sua singularidade, sem a necessidade de se assemelhar ou se comparar a alguém. Acreditamos que a verdadeira beleza reside na autenticidade e na capacidade de expressar a própria identidade através da moda. Queremos empoderar as mulheres para que se sintam confiantes em sua individualidade, valorizando suas características únicas e estilo pessoal.
+""")
+
+st.subheader('Produtos e Serviços em Detalhes')
+st.write("""
+Oferecemos uma coleção exclusiva de roupas femininas confeccionadas com tecidos naturais de alta qualidade, como linho, algodão e seda. Nossas peças são enriquecidas com detalhes feitos à mão, destacando o valor do artesanato e a atenção meticulosa aos acabamentos. Trabalhamos com peças-chave versáteis que se harmonizam facilmente com outros itens do guarda-roupa, proporcionando praticidade sem abrir mão da elegância.
+
+Entendemos a importância da personalização na moda. Por isso, damos às nossas clientes a possibilidade de escolher as cores que mais se identificam com seu estilo e preferência. Nosso atendimento é altamente personalizado, oferecendo consultas presenciais e online. Durante essas sessões, analisamos o tom de pele, o estilo pessoal e os desejos específicos de cada cliente, visando criar uma experiência de compra única e satisfatória.
+
+Além de disponibilizar roupas prontas para venda, também oferecemos a opção de criação de peças sob medida. Isso permite que nossas clientes participem ativamente do processo de design, resultando em peças exclusivas que refletem verdadeiramente sua personalidade e necessidades. Nosso compromisso é fornecer não apenas produtos de alta qualidade, mas também um serviço que valoriza e respeita a individualidade de cada mulher.
+
+Nosso objetivo final é criar uma marca que não apenas vista, mas também conecte e inspire. Queremos que cada mulher que use nossas roupas sinta-se confiante, bonita e autêntica, sabendo que está apoiando um projeto que valoriza a cultura e a expressão pessoal.
+""")
+
+st.subheader('A Essência de Salomé na Moda')
+st.write("""
+Ao vestir Salomé, a mulher abraça uma marca que celebra a sensualidade com elegância e respeito às raízes culturais. Inspirada na personagem bíblica Salomé, famosa por sua dança sedutora, nossa marca explora a feminilidade e a força que emanam dessa figura histórica. Salomé, conhecida por usar a arte da dança para expressar sua sensualidade, nos inspira a criar roupas que possibilitam uma mulher moderna ser confiante, sensual e, ao mesmo tempo, sofisticada. A marca busca resgatar e preservar a riqueza da herança cultural brasileira, evitando que ela se perca com o passar do tempo.
+
+A Salomé é mais do que uma marca de moda; é um movimento que incentiva as mulheres a serem fiéis a si mesmas, valorizando sua individualidade e potência. Encorajamos a quebra de padrões estéticos limitantes, mostrando que é possível ser feminina e forte, sensual e sofisticada, única e conectada com suas raízes.
+""")
+
+st.subheader('Coleção: Curvas de Movimento')
+st.write("""
+Inspirada na elegância e nas curvas voluptuosas do móvel clássico Bombe, esta coleção celebra a sensualidade e o movimento natural do corpo feminino. Assim como o design arredondado do Bombe valoriza o espaço e proporciona uma estética fluida, nossas peças são cuidadosamente criadas para abraçar as formas femininas, destacando suas curvas com graça e sutileza.
+
+Com tecidos que fluem e moldam o corpo, cada peça proporciona liberdade de movimento e uma sensação de leveza, permitindo que a mulher se sinta confiante e sensual. As silhuetas são pensadas para valorizar a figura, com cortes estratégicos que trazem à tona a essência de feminilidade e força, enquanto mantêm o conforto e a elegância.
+
+A coleção é uma celebração do design clássico com um toque contemporâneo, onde moda e arte se encontram para criar peças que falam diretamente à alma feminina, revelando e exaltando sua beleza natural.
+""")
+
+st.write("**Juliana Montemezzo** agradece aos professores do Centro Europeu pelo apoio e orientação durante a criação deste projeto, cuja visão e contribuição foram essenciais para o seu desenvolvimento.")
