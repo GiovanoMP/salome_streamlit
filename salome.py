a96d4fe1fee2db0964046f36afaf86c0cde5f481
Giovano M Panatta <162298143+GiovanoMP@users.noreply.github.com>
2024-09-19 13:03:33 -0300

    Update salome.py

--- salome.py
+++ salome.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.title('Salomé - Nicho, Segmento e Estilo')
 
 # Exibindo a imagem
-st.imagehttps:('https://raw.githubusercontent.com/GiovanoMP/salome_streamlit/main/imagem_salome.jpg')
+st.imagehttps:('https://github.com/GiovanoMP/salome_streamlit/blob/main/imagem_salome.jpg?raw=true', caption='Coleção Curvas de Movimento')
 # Texto principal do site
 st.subheader('Nicho, Segmento e Estilo')
 st.write("""
